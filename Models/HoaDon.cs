10b5fc85bc8bc017893cb92f638d70a71a932562
Your Name <you@example.com>
2022-07-04 01:16:37 +0700

    Add project files.

---
+++ Models/HoaDon.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace Restaurant.Models
+{
+    public partial class HoaDon
+    {
+        public HoaDon()
+        {
+            ChiTietHoaDons = new HashSet<ChiTietHoaDon>();
+        }
+
+        public int MaHoaDon { get; set; }
+        public int? MaKhachHang { get; set; }
+        public DateTime? NgayLap { get; set; }
+        public decimal? TongTien { get; set; }
+        public int? SoLuong { get; set; }
+        public string DiaChi { get; set; }
+        public string Sdt { get; set; }
+        public int? MaThanhToan { get; set; }
+        public int? MaVanChuyen { get; set; }
+
+        public virtual KhachHang MaKhachHangNavigation { get; set; }
+        public virtual ThanhToan MaThanhToanNavigation { get; set; }
+        public virtual VanChuyen MaVanChuyenNavigation { get; set; }
+        public virtual ICollection<ChiTietHoaDon> ChiTietHoaDons { get; set; }
+    }
+}
